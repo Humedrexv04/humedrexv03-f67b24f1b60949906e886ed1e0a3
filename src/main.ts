aae97bcb53645b6d423a6616954e81160cdad14d
humedrexv04 <humedrexv04@gmail.com>
2025-04-19 02:52:47 +0000

    first commit

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,13 @@ registerSwiperElements();
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
+  
+
 
